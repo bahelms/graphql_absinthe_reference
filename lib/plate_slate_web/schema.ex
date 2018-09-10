d0a32b465e7f4c6ab8025b46721270cb55450c48
jimbonk <jimbonkgit@gmail.com>
2018-09-09 21:53:23 -0400

    Splits schema into multiple modules

--- lib/plate_slate_web/schema.ex
+++ lib/plate_slate_web/schema.ex
@@ -1,81 +1,15 @@
-# ---
-# Excerpted from "Craft GraphQL APIs in Elixir with Absinthe",
-# published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material,
-# courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose.
-# Visit http://www.pragmaticprogrammer.com/titles/wwgraphql for more book information.
-# ---
 defmodule PlateSlateWeb.Schema do
   use Absinthe.Schema
-  alias PlateSlateWeb.Resolvers
+
+  import_types(__MODULE__.MenuTypes)
 
   query do
-    @desc "The list of available items on the menu!"
-    field :menu_items, list_of(:menu_item) do
-      arg(:filter, :menu_item_filter)
-      # enum values are passed in as all uppercase
-      arg(:order, type: :sort_order, default_value: :asc)
-      resolve(&Resolvers.Menu.menu_items/3)
-    end
-
-    @desc "List of current item categories"
-    field :categories, list_of(:category) do
-      arg(:matching, :string)
-      arg(:order, type: :sort_order, default_value: :asc)
-      resolve(&Resolvers.Menu.categories/3)
-    end
-  end
-
-  @desc "Item available on the menu"
-  object :menu_item do
-    field(:id, :id)
-
-    @desc "Name of item"
-    field(:name, :string)
-
-    @desc "Description of item"
-    field(:description, :string)
-
-    @desc "Current price of item"
-    field(:price, :integer)
-
-    @desc "Date added to menu"
-    field(:added_on, :date)
-  end
-
-  object :category do
-    field(:name, :string)
+    import_fields :menu_queries
   end
 
   enum :sort_order do
     value(:asc)
     value(:desc)
-  end
-
-  @desc "Filtering options for the menu item list"
-  input_object :menu_item_filter do
-    @desc "Matching a name"
-    field(:name, :string)
-
-    @desc "Matching a category name"
-    # when a filter object is provided, category is required
-    field(:category, non_null(:string))
-
-    @desc "Matching a tag"
-    field(:tag, :string)
-
-    @desc "Priced above a value"
-    field(:priced_above, :float)
-
-    @desc "Priced below a value"
-    field(:priced_below, :float)
-
-    @desc "Added to menu before this date"
-    field(:added_before, :date)
-
-    @desc "Added to menu after this date"
-    field(:added_after, :date)
   end
 
   scalar :date do
---
+++ lib/plate_slate_web/schema/menu_types.ex
@@ -0,0 +1,67 @@
+defmodule PlateSlateWeb.Schema.MenuTypes do
+  use Absinthe.Schema.Notation
+  alias PlateSlateWeb.Resolvers
+
+  object :menu_queries do
+    @desc "The list of available items on the menu"
+    field :menu_items, list_of(:menu_item) do
+      arg(:filter, :menu_item_filter)
+      # enum values are passed in as all uppercase
+      arg(:order, type: :sort_order, default_value: :asc)
+      resolve(&Resolvers.Menu.items/3)
+    end
+
+    @desc "List of current item categories"
+    field :categories, list_of(:category) do
+      arg(:matching, :string)
+      arg(:order, type: :sort_order, default_value: :asc)
+      resolve(&Resolvers.Menu.categories/3)
+    end
+  end
+
+  @desc "Item available on the menu"
+  object :menu_item do
+    field(:id, :id)
+
+    @desc "Name of item"
+    field(:name, :string)
+
+    @desc "Description of item"
+    field(:description, :string)
+
+    @desc "Current price of item"
+    field(:price, :integer)
+
+    @desc "Date added to menu"
+    field(:added_on, :date)
+  end
+
+  @desc "Filtering options for the menu item list"
+  input_object :menu_item_filter do
+    @desc "Matching a name"
+    field(:name, :string)
+
+    @desc "Matching a category name"
+    # when a filter object is provided, category is required
+    field(:category, non_null(:string))
+
+    @desc "Matching a tag"
+    field(:tag, :string)
+
+    @desc "Priced above a value"
+    field(:priced_above, :float)
+
+    @desc "Priced below a value"
+    field(:priced_below, :float)
+
+    @desc "Added to menu before this date"
+    field(:added_before, :date)
+
+    @desc "Added to menu after this date"
+    field(:added_after, :date)
+  end
+
+  object :category do
+    field(:name, :string)
+  end
+end
