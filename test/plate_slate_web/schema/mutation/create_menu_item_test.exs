c793db149eead9035687dd8e251e82dbc6f859d8
jimbonk <jimbonkgit@gmail.com>
2018-09-11 05:01:43 -0400

    Returns errors in tuple

--- test/plate_slate_web/schema/mutation/create_menu_item_test.exs
+++ test/plate_slate_web/schema/mutation/create_menu_item_test.exs
@@ -10,6 +10,7 @@ defmodule PlateSlateWeb.Schema.Query.CreateMenuItemTest do
       from(c in Menu.Category, select: c.id, where: c.name == "Sandwiches")
       |> Repo.one!()
       |> to_string()
+
     {:ok, category_id: category_id}
   end
 
@@ -29,15 +30,40 @@ defmodule PlateSlateWeb.Schema.Query.CreateMenuItemTest do
       "price" => "5.75",
       "categoryId" => cat_id
     }
+
     response = post(conn, "/api", query: @query, variables: %{menuItem: menu_item})
+
     assert json_response(response, 200) == %{
-      "data" => %{
-        "createMenuItem" => %{
-          "name" => menu_item["name"],
-          "description" => menu_item["description"],
-          "price" => menu_item["price"],
+             "data" => %{
+               "createMenuItem" => %{
+                 "name" => menu_item["name"],
+                 "description" => menu_item["description"],
+                 "price" => menu_item["price"]
+               }
+             }
+           }
+  end
+
+  test "creating a menu item with an existing name fails", %{conn: conn, category_id: cat_id} do
+    menu_item = %{
+      "name" => "Reuben",
+      "description" => "Roast beef, caramelized onions, horseradish, ...",
+      "price" => "5.75",
+      "categoryId" => cat_id
+    }
+
+    response = post(conn, "/api", query: @query, variables: %{menuItem: menu_item})
+
+    assert json_response(response, 200) == %{
+      "data" => %{"createMenuItem" => nil},
+      "errors" => [
+        %{
+          "locations" => [%{"column" => 0, "line" => 2}],
+          "message" => "Could not create menu item",
+          "details" => %{"name" => ["has already been taken"]},
+          "path" => ["createMenuItem"],
         }
-      }
+      ]
     }
   end
 end
